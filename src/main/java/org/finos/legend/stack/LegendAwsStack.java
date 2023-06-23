22c6a9e6a41863a49ae4ae341bb10e81b72a29c6
Nicholas Hofbauer <nicholas.hofbauer@gmail.com>
2023-06-23 12:53:10 -0400

    Update image

--- src/main/java/org/finos/legend/stack/LegendAwsStack.java
+++ src/main/java/org/finos/legend/stack/LegendAwsStack.java
@@ -1,8 +1,14 @@
 package org.finos.legend.stack;
 
 import software.constructs.Construct;
+import software.amazon.awscdk.CfnOutput;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
 
 public class LegendAwsStack extends Stack {
     public LegendAwsStack(final Construct scope, final String id) {
@@ -12,6 +18,30 @@ public class LegendAwsStack extends Stack {
     public LegendAwsStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        //TODO: Add ECS Service and ALB
+        Vpc vpc = Vpc.Builder
+            .create(this, "LegendAwsVpc")
+            .maxAzs(3)
+            .build();
+
+        Cluster cluster = Cluster.Builder
+            .create(this, "LegendAwsCluster")
+            .vpc(vpc)
+            .build();
+
+        ApplicationLoadBalancedFargateService service = ApplicationLoadBalancedFargateService.Builder
+            .create(this, "LegendAwsService")
+            .cluster(cluster)
+            .cpu(2048)
+            .desiredCount(1)
+            .taskImageOptions(
+                ApplicationLoadBalancedTaskImageOptions
+                    .builder()
+                    .image(ContainerImage.fromRegistry("finos/legend-omnibus:latest-slim"))
+                    .build())
+            .memoryLimitMiB(4096)
+            .publicLoadBalancer(true)
+            .build();
+
+        CfnOutput.Builder.create(this, "LegendAwsServiceUrl").value(service.getLoadBalancer().getLoadBalancerDnsName()).build();
     }
 }
