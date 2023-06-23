06293b223feba989756119eee403d253ed0fb24f
Nicholas Hofbauer <nicholas.hofbauer@gmail.com>
2023-06-23 14:26:28 -0400

    Update cfn parameter name

--- src/main/java/org/finos/legend/stack/LegendAwsStack.java
+++ src/main/java/org/finos/legend/stack/LegendAwsStack.java
@@ -1,7 +1,5 @@
 package org.finos.legend.stack;
 
-import software.constructs.Construct;
-
 import java.util.Map;
 
 import software.amazon.awscdk.CfnOutput;
@@ -13,6 +11,7 @@ import software.amazon.awscdk.services.ecs.Cluster;
 import software.amazon.awscdk.services.ecs.ContainerImage;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.constructs.Construct;
 
 public class LegendAwsStack extends Stack {
     public LegendAwsStack(final Construct scope, final String id) {
@@ -22,7 +21,7 @@ public class LegendAwsStack extends Stack {
     public LegendAwsStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        CfnParameter parameter = CfnParameter.Builder.create(this, "LEGEND_OMNIBUS_REMOTE_GITLAB_PAT").type("String").build();
+        CfnParameter gitlabParameter = CfnParameter.Builder.create(this, "gitlab-pat").type("String").build();
 
         Vpc vpc = Vpc.Builder
             .create(this, "LegendAwsVpc")
@@ -43,7 +42,7 @@ public class LegendAwsStack extends Stack {
                 ApplicationLoadBalancedTaskImageOptions
                     .builder()
                     .image(ContainerImage.fromRegistry("finos/legend-omnibus:latest-slim"))
-                    .environment(Map.of("LEGEND_OMNIBUS_NGINX_PORT", "80", "LEGEND_OMNIBUS_REMOTE_GITLAB_PAT", parameter.getValueAsString()))
+                    .environment(Map.of("LEGEND_OMNIBUS_NGINX_PORT", "80", "LEGEND_OMNIBUS_REMOTE_GITLAB_PAT", gitlabParameter.getValueAsString()))
                     .build())
             .memoryLimitMiB(4096)
             .publicLoadBalancer(true)
